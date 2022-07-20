28e670f42d5767fe66d59d183521e4a6efb84c95
zhouboyi <1144188685@qq.com>
2022-07-20 23:40:46 +0800

    chore(orm): 添加diesel依赖，使用diesel包操作SQLite数据库

---
+++ src/card.rs
@@ -0,0 +1,27 @@
+use crate::schema::note_card;
+use rocket::serde::{Deserialize, Serialize};
+
+// 标准 Model
+#[derive(Queryable, Deserialize, Serialize, AsChangeset)]
+#[serde(crate = "rocket::serde")]
+#[table_name = "note_card"]
+pub struct Card {
+    pub id: i32,
+    pub title: String,
+    pub content: String,
+    pub tip: String,
+    pub extra: String,
+    #[serde(skip_deserializing)]
+    pub create_time: String,
+}
+
+// Request Body Model
+#[derive(Insertable, Deserialize)]
+#[serde(crate = "rocket::serde")]
+#[table_name = "note_card"]
+pub struct NewCard {
+    pub title: String,
+    pub content: String,
+    pub tip: String,
+    pub extra: String,
+}
