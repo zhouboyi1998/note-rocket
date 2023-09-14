575c05a73ea051a99b1deff7642ff80bc796be27
zhouboyi <1144188685@qq.com>
2023-09-14 15:34:34 +0800

    style(code): 格式化代码

--- src/repository/card.rs
+++ src/repository/card.rs
@@ -1,31 +1,36 @@
-use diesel::{
-    query_dsl::methods::{FindDsl, LimitDsl, OrderDsl},
-    ExpressionMethods, QueryResult, RunQueryDsl, SqliteConnection,
-};
+use diesel::{ExpressionMethods, query_dsl::methods::{FindDsl, LimitDsl, OrderDsl}, QueryResult, RunQueryDsl, SqliteConnection};
 
 use crate::model::card::{Card, NewCard};
 use crate::schema::note_card;
 
-pub struct CardRepositiry;
+pub struct CardRepository;
 
-impl CardRepositiry {
+impl CardRepository {
     pub fn list(c: &SqliteConnection) -> QueryResult<Vec<Card>> {
-        note_card::table.limit(100).load::<Card>(c)
+        note_card::table
+            .limit(100)
+            .load::<Card>(c)
     }
 
     pub fn one(c: &SqliteConnection, id: i32) -> QueryResult<Card> {
-        note_card::table.find(id).get_result::<Card>(c)
+        note_card::table
+            .find(id)
+            .get_result::<Card>(c)
     }
 
     pub fn insert(c: &SqliteConnection, new_card: NewCard) -> QueryResult<Card> {
         diesel::insert_into(note_card::table)
             .values(new_card)
             .execute(c)?;
-        note_card::table.order(note_card::id.desc()).first(c)
+
+        note_card::table
+            .order(note_card::id.desc())
+            .first(c)
     }
 
     pub fn update(c: &SqliteConnection, card: Card) -> QueryResult<Card> {
-        diesel::update(note_card::table.find(card.id))
+        diesel::update(note_card::table
+            .find(card.id))
             .set((
                 note_card::title.eq(card.title.to_owned()),
                 note_card::content.eq(card.content.to_owned()),
@@ -33,10 +38,14 @@ impl CardRepositiry {
                 note_card::extra.eq(card.extra.to_owned()),
             ))
             .execute(c)?;
-        note_card::table.find(card.id).get_result::<Card>(c)
+
+        note_card::table
+            .find(card.id)
+            .get_result::<Card>(c)
     }
 
     pub fn delete(c: &SqliteConnection, id: i32) -> QueryResult<usize> {
-        diesel::delete(note_card::table.find(id)).execute(c)
+        diesel::delete(note_card::table.find(id))
+            .execute(c)
     }
 }
